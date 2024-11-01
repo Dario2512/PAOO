69d0241e697b5ba297ccd3e3e4cca3d4a0b334e8
Dario2512 <dario.friskan@student.upt.ro>
2024-11-01 18:49:23 +0200

    Tema 1.1

---
+++ build/CMakeFiles/AudiA3App.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/dario/Desktop/PAOO/Car.cpp" "CMakeFiles/AudiA3App.dir/Car.cpp.o" "gcc" "CMakeFiles/AudiA3App.dir/Car.cpp.o.d"
+  "/home/dario/Desktop/PAOO/main.cpp" "CMakeFiles/AudiA3App.dir/main.cpp.o" "gcc" "CMakeFiles/AudiA3App.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/AudiA3App.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "AudiA3App"
+  "AudiA3App.pdb"
+  "CMakeFiles/AudiA3App.dir/Car.cpp.o"
+  "CMakeFiles/AudiA3App.dir/Car.cpp.o.d"
+  "CMakeFiles/AudiA3App.dir/main.cpp.o"
+  "CMakeFiles/AudiA3App.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AudiA3App.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
